0af5d2401770e9c3dd79db38ae1633d0f2152c71
Chris Lam <whoischrislam@gmail.com>
2023-02-16 15:50:49 -0800

    Updated the cover with flex. Refactored CSS.

--- components/Cover.jsx
+++ components/Cover.jsx
@@ -3,17 +3,14 @@ import React from "react"
 export default function Cover(){
     return (
         <div className="cover">
-            <div className="filter"></div>
-                <div className="cover-wrapper">
-                    <img src="../tfmars.png"/>
-                    <img src="../dead.png"/>
-                    <img src="../dune.png"/>
-                    <img src="../quacks.png"/>
-                    <img src="../root.png"/>
-                    <img src="../tapestry.png"/>
-                    <img src="../viticulture.png"/>
-                    <img src="../wingspan.png"/>
-                </div>
+            <img src="../tfmars.png"/>
+            <img src="../dead.png"/>
+            <img src="../dune.png"/>
+            <img src="../quacks.png"/>
+            <img src="../root.png"/>
+            <img src="../tapestry.png"/>
+            <img src="../viticulture.png"/>
+            <img src="../wingspan.png"/>
         </div>
     );
 }
